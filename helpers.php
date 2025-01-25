43524da3d42b5415121f22a5ba6c3c68ab597fcf
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2025-01-24 23:26:28 -0700

    create partial fucntion and loadview func

--- helpers.php
+++ helpers.php
@@ -7,7 +7,33 @@
  * @return string
  */
 
- function basePath($path = '') {
+function basePath($path = '')
+{
     // echo $path;
-     return __DIR__ . '/' . $path;
- }
+    return __DIR__ . '/' . $path;
+}
+
+/**
+ * Load a view 
+ * @param string $name
+ * @return void
+ */
+
+function loadView($name)
+{
+    $viewPath = basePath("views/{$name}.view.php");
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View '{$name}' not found";
+    }
+}
+function loadPartial($name)
+{
+   $partialPath =  basePath("views/partials/{$name}.php");
+   if (file_exists($partialPath)) {
+    require $partialPath;
+} else {
+    echo "Partial '{$name}' not found";
+}
+}
