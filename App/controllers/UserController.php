c9724fd85d9cb17d6f2c1b16107ae0ee1fe2c7a4
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2025-01-29 10:38:26 -0700

    create session

--- App/controllers/UserController.php
+++ App/controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use Framework\Database;
 use Framework\Validation;
+use Framework\Session;
 
 
 class UserController
@@ -98,8 +99,16 @@ class UserController
         $this->db->query("INSERT INTO users (name, email, city, state, password) VALUES (:name, :email, :city, :state, :password)", $params);
 
 
-        redirect('/auth/login');
+        //// Get new user id
+        $userId = $this->db->conn->lastInsertId();
+        Session::set('user', [
+            'id' => $userId,
+            'name' => $name,
+            'email' => $email,
+            'city' => $city,
+            'state' => $state,
+        ]);
 
-        inspectAndDie('store');
+        redirect('/auth/login');
     }
 }
