a81522058a53dd797ebd8b30a2354f05024a0423
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2025-02-07 09:05:14 -0700

    fixed bug

--- App/views/auth/login.view.php
+++ App/views/auth/login.view.php
@@ -28,7 +28,7 @@
 
             <p class="mt-4 text-gray-500">
                 Don't have an account?
-                <a class="text-blue-900" href="/auth/create">Register</a>
+                <a class="text-blue-900" href="/auth/register">Register</a>
             </p>
         </form>
     </div>
